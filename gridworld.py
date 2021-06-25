e8aec32af65f5eb703e3cfc3c42cd072ed5c7342
edluffy <edward@ncade.com>
2021-06-25 10:11:17 +0100

    simplify env

--- gridworld.py
+++ gridworld.py
@@ -3,32 +3,43 @@ import numpy as np
 # A Markov Decision Process: <S, A, P, R, Gamma>
 class GridWorld():
     def __init__(self):
-        self.states = np.array([[0, 0], [0, 1], [0, 2], [0, 3],
-                                [1, 0], [1, 1], [1, 2], [1, 3],
-                                [2, 0], [2, 1], [2, 2], [2, 3]])
+        self.states = [[0, 0], [1, 0], [2, 0], [3, 0],
+                       [0, 1], [1, 1], [2, 1], [3, 1],
+                       [0, 2], [1, 2], [2, 2], [3, 2]]
 
-        self.actions = np.array(['U','D','L','R'])
+        self.actions = ['U','D','L','R']
 
         self.transition_pr = None # Leave blank if fully deterministic
 
-        self.rewards = np.array([0, 0, 0,  1,
-                                 0, 0, 0, -1,
-                                 0, 0, 0,  0])
+        self.rewards = [0, 0, 0,  1,
+                        0, 0, 0, -1,
+                        0, 0, 0,  0]
+
         self.gamma = 0.99
 
-        self.current_state = self.states[0]
+        self.cstate = self.states[0]
 
     def sample(self, action):
-        if action in self.actions:
-            # wrap around if action causes border collision
-            if (self.current_state[0] == 0 and action == 'U') \
-                    or (self.current_state[0] == 1 and action == 'D') \
-                    or (self.current_state[1] == 0 and action == 'L') \
-                    or (self.current_state[1] == 3 and action == 'R'):
-                next_state = self.current_state
-        ##return obs, reward, done
+        if action not in self.actions:
+            print('Unknown action')
 
+        nstate = self.cstate
+
+        if action == 'U' and nstate[1] > 0:
+            nstate[1] = nstate[1]-1
+        elif action == 'D' and nstate[1] < 2:
+            nstate[1] = nstate[1]+1
+        elif action == 'L' and nstate[0] > 0:
+            nstate[0] = nstate[0]-1
+        elif action == 'R' and nstate[0] < 3:
+            nstate[0] = nstate[0]+1
+
+        obs = nstate
+        reward = self.rewards[self.states.index(nstate)]
+        done = (nstate == [3, 0])
+
+        return obs, reward, done
 
 env = GridWorld()
 
-env.sample('U')
+print(env.sample('U'))
