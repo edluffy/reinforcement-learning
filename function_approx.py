be4da78d6a8affa42de1036bf5e4b55afe8a7cb9
edluffy <edward@ncade.com>
2021-07-08 17:56:51 +0100

    split training

--- function_approx.py
+++ function_approx.py
@@ -5,23 +5,17 @@ argmax = lambda d: max(d, key=d.get)
 
 def vfa_monte_carlo(env, gamma=0.9, alpha=0.1, ep=10):
     obs = env.reset()
-    w = np.random.random(4)
+    w = np.random.random(5)
 
     # Generate episode
     episode = [] # [(x0, a0, r1), (x1, a1, r2),...]
     for n in range(ep):
-        for t in range(100000):
-            env.render()
-
-            action = 0 if np.dot(obs, w) < 0 else 1
-
-            #print(action)
-
+        while True:
+            action = env.action_space.sample()
             obs, reward, done, _ = env.step(action)
             episode.append((obs, action, reward))
 
             if done:
-                print('STEPS:', t)
                 obs = env.reset()
                 break
 
@@ -29,15 +23,30 @@ def vfa_monte_carlo(env, gamma=0.9, alpha=0.1, ep=10):
         #        x: feature vector for current state and action
         G = 0
         for (obs, action, reward) in reversed(episode):
-            x = obs
+            x = np.append(obs, action)
             G = gamma*G + reward
             Q_approx = np.dot(x, w)
             w += alpha*(G-Q_approx)*x
-        print('MSE:', (G-Q_approx)**2)
+        #print('MSE:', (G-Q_approx)**2)
+    print(w)
+
+    for n in range(1):
+        for t in range(100000):
+            env.render()
+            action = argmax({a: np.dot(np.append(obs, a), w) for a in [0, 1]})
+
+            obs, reward, done, _ = env.step(action)
+
+            if done:
+                print('Episode', n, 't:', t)
+                obs = env.reset()
+                break
 
     env.close()
+    
 
 
 env = gym.make("CartPole-v1")
-vfa_monte_carlo(env, ep=2000)
+for _ in range(10):
+    vfa_monte_carlo(env, ep=200)
 
